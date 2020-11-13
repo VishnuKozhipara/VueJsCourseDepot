59ad967e49d280562a9278076ca614ea8bcc9535
Vishnu <vishnu.k.1998@gmail.com>
2020-11-13 19:40:32 +0400

    Added the remaining subjects

--- lesson.js
+++ lesson.js
@@ -8,5 +8,104 @@ let lesson = [{
     spaces: 5,
     rating: 3,
     quantity: 5
+},
+{
+    id: 102,
+    subject: 'English',
+    teacher: 'Willam Shakespeare III',
+    location: 'Stratford-upon-Avon',
+    price: 25,
+    image: "english2.png",
+    spaces: 5,
+    rating: 3,
+    quantity: 5    
+},
+{
+    id: 103,
+    subject: 'Science',
+    teacher:'Bill Nye the Science Guy',
+    location: ' Washington United States',
+    price: 25,
+    image: "scienceguy.png",
+    spaces: 5,
+    rating: 3,
+    quantity: 5    
+},
+{
+    id: 104,
+    subject: 'Electrical Engineering',
+    teacher:'Mehdi Sadaghdar (ElectroBoom)',
+    location: 'Canada',
+    price: 35.5,
+    image: "electrical.jpg",
+    spaces: 5,
+    rating: 3,
+    quantity: 5    
+},
+{
+    id: 105,
+    subject: 'French',
+    teacher: 'Napoleon Bonaparte ',
+    location: 'Ajaccio, France',
+    price: 50,
+    image: "frencha.png",
+    spaces: 5,
+    rating: 3,
+    quantity: 5    
+},
+{
+    id: 106,
+    subject: 'Business',
+    teacher: 'Fromer President Donald John Trump',
+    location: 'New York',
+    price: 1000000,
+    image: "business.jpg",
+    spaces: 5,
+    rating: 3,
+    quantity: 5    
+},
+{
+    id: 107,
+    subject: 'Astrophysics',
+    teacher: 'Neil deGrasse Tyson',
+    location: 'New York',
+    price: 75,
+    image: "astrophysics.png",
+    spaces: 5,
+    rating: 3,
+    quantity: 5    
+},
+{
+    id: 108,
+    subject: 'Music',
+    teacher:'645AR',
+    location: 'New York',
+    price: 69,
+    image: "music.png",
+    spaces: 5,
+    rating: 3,
+    quantity: 5    
+},
+{
+    id: 109,
+    subject: 'Aerospace Engineering',
+    teacher: 'Elon Musk',
+    location: 'New York',
+    price: 420.69,
+    image: "aero.png",
+    spaces: 5,
+    rating: 3,
+    quantity: 5    
+},
+{
+    id: 110,
+    subject: 'Political Science',
+    teacher: 'Joseph Biden II',
+    location: 'New York',
+    price: 50,
+    image: "politics.png",
+    spaces: 5,
+    rating: 3,
+    quantity: 5    
 }
 ]
