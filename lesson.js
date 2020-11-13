9ef822f7050aabdcb9ab2032d98eff88d7981fb1
Vishnu <vishnu.k.1998@gmail.com>
2020-11-13 19:14:23 +0400

    Added lesson to js file where the attributes are stored

---
+++ lesson.js
@@ -0,0 +1,12 @@
+let lesson = [{
+    id: 101,
+    subject: 'Math',
+    teacher:'Bettle Juice',
+    location: 'London',
+    price: 30,
+    image: "math2.png",
+    spaces: 5,
+    rating: 3,
+    quantity: 5
+}
+]
